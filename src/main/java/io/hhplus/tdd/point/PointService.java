cbafe7d71448fb56e5edde5d19cd25c597f07faf
tre2man <kimtree3940@gmail.com>
2025-07-11 00:27:27 +0900

    feature: addAmount, usedAmount 예외 처리

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -2,9 +2,11 @@ package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PointService {
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
@@ -18,18 +20,26 @@ public class PointService {
         return userPointTable.selectById(userId);
     }
 
-    void chargePoint(long userId, long addAmount) {
+    UserPoint chargePoint(long userId, long addAmount) {
+        if (addAmount < 0) {
+            throw new IllegalArgumentException("충전 금액은 양수여야 합니다.");
+        }
         long currentPoint = userPointTable.selectById(userId).point();
         long newPoint = currentPoint + addAmount;
         userPointTable.insertOrUpdate(userId, newPoint);
         pointHistoryTable.insert(userId, addAmount, TransactionType.CHARGE, System.currentTimeMillis());
+        return userPointTable.selectById(userId);
     }
 
-    void usePoint(long userId, long useAmount) {
+    UserPoint usePoint(long userId, long useAmount) {
+        if (useAmount < 0) {
+            throw new IllegalArgumentException("사용 금액은 양수여야 합니다.");
+        }
         long currentPoint = userPointTable.selectById(userId).point();
         long newPoint = currentPoint - useAmount;
         userPointTable.insertOrUpdate(userId, newPoint);
         pointHistoryTable.insert(userId, useAmount, TransactionType.USE, System.currentTimeMillis());
+        return userPointTable.selectById(userId);
     }
 
     List<PointHistory> getPointHistories(long userId) {
