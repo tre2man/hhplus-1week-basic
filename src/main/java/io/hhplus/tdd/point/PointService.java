2ad2e39de34961d9c7d2487d4356153572896254
tre2man <kimtree3940@gmail.com>
2025-07-10 22:03:41 +0900

    feature: PointService

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,32 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+
+public class PointService {
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
+        this.userPointTable = userPointTable;
+        this.pointHistoryTable = pointHistoryTable;
+    }
+
+    UserPoint getUserPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
+
+    void chargePoint(long userId, long addAmount) {
+        long currentPoint = userPointTable.selectById(userId).point();
+        long newPoint = currentPoint + addAmount;
+        userPointTable.insertOrUpdate(userId, newPoint);
+        pointHistoryTable.insert(userId, addAmount, TransactionType.CHARGE, System.currentTimeMillis());
+    }
+
+    void usePoint(long userId, long useAmount) {
+        long currentPoint = userPointTable.selectById(userId).point();
+        long newPoint = currentPoint - useAmount;
+        userPointTable.insertOrUpdate(userId, newPoint);
+        pointHistoryTable.insert(userId, useAmount, TransactionType.USE, System.currentTimeMillis());
+    }
+}
